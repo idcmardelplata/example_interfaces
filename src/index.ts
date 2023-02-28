f9c6483e62540043112ea2a1a13252499df67af0
Martin Algañaraz <idcmardelplata@gmail.com>
2023-02-28 12:45:41 -0300

    build:(scafold) Basic scafold for typescript

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+class Customer {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  store(): boolean {
+    console.log(`the customer ${this.name} was stored sucefully`);
+    return true;
+  }
+}
+
+class Seller {
+   name: string;
+   age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  store(): boolean {
+    console.log(`the seller ${this.name} was stored sucefully`);
+    return true;
+  }
+}
