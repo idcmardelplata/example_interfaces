10ea83441ddbbb602f8c32c77e72c3623ebdac57
SofiaMartinez-Ramovecchi <zunildaramivecchi@gmail.com>
2023-03-01 00:46:10 -0300

    Added imports and translated
    feat     - use when add new code features

--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,16 @@
-interface Person {
-  /* name: string; */
-  /* age: number; */
-  store(): boolean; 
-};
-
-class Customer implements Person {
-  name: string;
-  age: number;
-
-  constructor(name: string, age: number) {
-    this.name = name;
-    this.age = age;
-  }
-
-  store(): boolean {
-    console.log(`the customer ${this.name} was stored sucefully`);
-    return true;
-  }
-}
-
-class Seller implements Person {
-   name: string;
-   age: number;
-
-  constructor(name: string, age: number) {
-    this.name = name;
-    this.age = age;
-  }
-
-  store(): boolean {
-    console.log(`the seller ${this.name} was stored sucefully`);
-    return true;
-  }
-}
-
-class Usuario implements Person {
-  constructor() {
-  }
-
-  store(): boolean {
-    console.log("salvado");
-    return true;
-  }
-}
-
-class DB implements Person {
-  store() : boolean {
-    console.log("salvado en la base de datos");
-    return true;
-  }
-}
-
+import { Customer } from "./customer";
+import { Seller } from "./seller";
+import { User } from "./user";
+import { Person } from "./person";
 
 function storeInDB(user: Person) {
   user.store();
 }
 
-
 let customer = new Customer("carlos", 23);
 let seller = new Seller("ruben", 42);
-let usuario = new Usuario();
+let user = new User();
 
 storeInDB(customer);
 storeInDB(seller);
-storeInDB(usuario);
+storeInDB(user);
