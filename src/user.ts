7b7438dd0e1dacbf3af46212265db6f9c6f95c5e
SofiaMartinez-Ramovecchi <zunildaramivecchi@gmail.com>
2023-03-01 11:02:57 -0300

    fix: repair class and import file

--- src/user.ts
+++ src/user.ts
@@ -1,5 +1,13 @@
-class User implements Person {
-  constructor() {
+import { Person } from "./person";
+
+export class User implements Person {
+
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
   }
 
   store(): boolean {
