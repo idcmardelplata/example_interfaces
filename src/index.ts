74e31ec67e7a1ae9ed95bb135d770778f766d20a
SofiaMartinez-Ramovecchi <zunildaramivecchi@gmail.com>
2023-03-01 11:00:24 -0300

    fix: repair class for it function in separated file

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
+import { Person } from "./person";
 import { Customer } from "./customer";
 import { Seller } from "./seller";
 import { User } from "./user";
-import { Person } from "./person";
 
-function storeInDB(user: Person) {
-  user.store();
+export function storeInDB(user: Person): boolean {
+  return user.store();
 }
 
 let customer = new Customer("carlos", 23);
+let customer1= new Customer("Nicolas", 24);
 let seller = new Seller("ruben", 42);
-let user = new User();
-
+let user = new User("pepe", 22);
+let user1 = new User("maria", 33);
 storeInDB(customer);
 storeInDB(seller);
 storeInDB(user);
